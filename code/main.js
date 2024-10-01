fbda3c95d89f802b583b0ea47e32ce2b0add205e
IdrealDev <idreal.dev@proton.me>
2024-10-01 20:26:23 +0200

    Basisdatein hinzugefügt + erste functions definiert

---
+++ code/main.js
@@ -0,0 +1,18 @@
+
+function init(){
+
+}
+
+function generateGalaxy(){
+
+}
+
+function createPlanet(){
+    var planet = {
+        name: "Planet",
+        production: Math.random()*10+5,        
+        attackRating: Math.random()*0.4+0.3
+    }
+
+
+}
